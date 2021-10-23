790bca7cbc2775d214421476ba3229bba062cd25
Abhishek <abhi001574@gmail.com>
2021-10-23 10:12:39 +0530

    Add order table and place order

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,15 @@
 const homeController = require('../app/http/controllers/homeController');
 const cartController = require('../app/http/controllers/customers/cartController');
 const authController = require('../app/http/controllers/authcontroller');
+const orderController = require('../app/http/controllers/customers/orderController')
+const AdminOrderController = require('../app/http/controllers/admin/orderController');
+
+//Middlewares
+const auth = require('../app/http/middlewares/auth')
 const guest = require('../app/http/middlewares/guest')
+const admin = require('../app/http/middlewares/admin');
+
+
 
 function initRoutes(app){
 //home routes  
@@ -22,6 +30,13 @@ function initRoutes(app){
 
 // update-cart
     app.post('/update-cart', cartController().update)
+
+// Customer Routes 
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders', auth, orderController().index)
+
+// Admin routes
+    app.get('/admin/orders', admin, AdminOrderController().index)
 
 }
 
